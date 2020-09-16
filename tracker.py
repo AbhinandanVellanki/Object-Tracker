d9a913f4efbc0df9391b6e670e3051a297821281
AbhinandanVellanki <abhinandan.vellanki@gmail.com>
2020-09-16 17:15:24 +0800

    fixed bugs

--- tracker.py
+++ tracker.py
@@ -2,10 +2,10 @@
 #author: abhinandan.vellanki@gmail.com
 
 #import the necessary packages
-from imutils.video import VideoStream
-from imutils.video import FPS
+#from imutils.video import VideoStream
+#from imutils.video import FPS
 import argparse
-import imutils
+#import imutils
 import time
 import cv2
 
@@ -25,39 +25,38 @@ class track():
         (major, minor) = cv2.__version__.split(".")[:2]
 
         #if version <=3.2, use special factory function to create object tracker
-        if int(major) == 3 and int(minor) <3:
-           self.tracker=cv2.Tracker_create(self.tracker_name.upper())
-
-        #if version >=3.3, need to explicitly call tracker constructor
-        else:
-            OPENCV_TRACKERS={ #mapper, does not include GOTURN
-                "csrt": cv2.TrackerCSRT_create,
-                "kcf": cv2.TrackerKCF_create,
-                "boosting": cv2.TrackerBoosting_create,
-                "mil": cv2.TrackerMIL_create,
-                "tld": cv2.TrackerTLD_create,
-                "medianflow": cv2.TrackerMedianFlow_create,
-                "mosse": cv2.TrackerMOSSE_create
-            }
-            self.tracker=OPENCV_TRACKERS[self.tracker_name]() #call constructor at runtime
+        # if int(major) == 3 and int(minor) <3:
+        #    self.tracker=cv2.Tracker_create(self.tracker_name.upper())
+           
+        # else:#if version >=3.3, need to explicitly call tracker constructor
+        OPENCV_TRACKERS={ #mapper, does not include GOTURN
+            "csrt": cv2.TrackerCSRT_create,
+            "kcf": cv2.TrackerKCF_create,
+            "boosting": cv2.TrackerBoosting_create,
+            "mil": cv2.TrackerMIL_create,
+            "tld": cv2.TrackerTLD_create,
+            "medianflow": cv2.TrackerMedianFlow_create,
+            "mosse": cv2.TrackerMOSSE_create
+        }
+        self.tracker=OPENCV_TRACKERS[self.tracker_name]() #call constructor at runtime
 
         #initialize bounding box coordinates to track
         BBtrack = None
 
         #if no video path, choose webcam
-        if self.video == None:
-            print("Starting video capture...")
-            try:
-                self.vs=VideoStream(src=0).start()
-                time.sleep(1.0)#time to start stream
-            except:
-                print("Unable to capture video from webcam!!")
-                self.vs=None
-        else:
-            self.vs=cv2.VideoCapture(self.video)
+	    # if self.video == None:
+	    #     print("Starting video capture...")
+	    #     try:
+        #         self.vs=VideoStream(src=0).start()
+        #         time.sleep(1.0)#time to start stream
+        #     except:
+        #         print("Unable to capture video from webcam!!")
+        #         self.vs=None
+        # else:
+        self.vs=cv2.VideoCapture(self.video)
 
         #initialize fps throughput estimator
-        self.fps=None
+        #self.fps=None
     
     def start(self):
         #iterate over video frames
@@ -69,7 +68,8 @@ class track():
                 break;
             
             #resize for faster processing and get dimensions
-            frame=imutils.resize(frame, width=500)
+            #frame=imutils.resize(frame, width=500)
+            frame = cv2.resize(frame,)
             (H,W) = frame.shape[:2]
             
             #if an object is being tracked
@@ -81,14 +81,14 @@ class track():
                     cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2) #draw green rectangle around new coordinates of tracking box
                     
                 #update fps counter
-                self.fps.update()
-                self.fps.stop()
+                #self.fps.update()
+                #self.fps.stop()
                 
                 #initialize info to be diplayed on frame
                 info=[
                     ("Tracker", self.tracker),
-                    ("Success", "Yes" if success else "No"),
-                    ("FPS", "{:.2f}".format(fps.fps()))                    
+                    ("Success", "Yes" if success else "No")
+                    #("FPS", "{:.2f}".format(fps.fps()))                    
                 ]
                 
                 #loop over info and add to frame
@@ -107,19 +107,19 @@ class track():
                 
                 #start object tracker on the supplied bounding box, start FPS throughput estimator
                 self.tracker.init(frame, BBtrack)
-                self.fps=FPS().start()
+                #self.fps=FPS().start()
             
             #if 'q' key is pressed, break
             elif key==ord("q"):
                 break
             
         if self.video == None:
-            self.vs.stop()#release webcam pointer
+            self.vs.stop() #release webcam pointer
         else:
-            self.vs.release()#release file pointer
+            self.vs.release() #release file pointer
             
         cv2.destroyAllWindows()
     
 if __name__ == "__main__":
-	obj_tracker=track("~/Desktop/cv2_test.mp4", "csrt")
-        obj_tracker.start()
+    obj_tracker=track("~/Desktop/cv2_test.mp4", "csrt")
+    obj_tracker.start()
