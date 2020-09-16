1dd313fb6b66fd9f2dfd9c73d894cf1837773fae
robot <garychoi1999@gmail.com>
2020-09-16 15:53:38 +0800

    fixed bugs

--- tracker.py
+++ tracker.py
@@ -19,14 +19,14 @@ class track():
         # args=vars(ap.parse_args())
         
         self.video = video
-        self.tracker = tracker
+        self.tracker_name = tracker
         
         #extract OpenCV version info
         (major, minor) = cv2.__version__.split(".")[:2]
 
         #if version <=3.2, use special factory function to create object tracker
         if int(major) == 3 and int(minor) <3:
-            tracker=cv2.Tracker_create(self.tracker.upper())
+           self.tracker=cv2.Tracker_create(self.tracker_name.upper())
 
         #if version >=3.3, need to explicitly call tracker constructor
         else:
@@ -39,7 +39,7 @@ class track():
                 "medianflow": cv2.TrackerMedianFlow_create,
                 "mosse": cv2.TrackerMOSSE_create
             }
-            tracker=OPENCV_TRACKERS[self.tracker]() #call constructor at runtime
+            self.tracker=OPENCV_TRACKERS[self.tracker_name]() #call constructor at runtime
 
         #initialize bounding box coordinates to track
         BBtrack = None
@@ -48,21 +48,21 @@ class track():
         if self.video == None:
             print("Starting video capture...")
             try:
-                vs=VideoStream(src=0).start()
+                self.vs=VideoStream(src=0).start()
                 time.sleep(1.0)#time to start stream
             except:
                 print("Unable to capture video from webcam!!")
-                vs=None
+                self.vs=None
         else:
-            vs=cv2.VideoCapture(self.video)
+            self.vs=cv2.VideoCapture(self.video)
 
         #initialize fps throughput estimator
-        fps=None
+        self.fps=None
     
-    def start():
+    def start(self):
         #iterate over video frames
         while True:
-            frame=vs.read()
+            frame=self.vs.read()
             frame=frame[1] if not self.video == None else frame #handle separate cases for VideoStream or VideoCapture
             
             if frame is None:#reached end of stream
@@ -74,15 +74,15 @@ class track():
             
             #if an object is being tracked
             if BBtrack is not None:
-                (success, box) = tracker.update(frame)#get new BB coordinates of selected box
+                (success, box) = self.tracker.update(frame)#get new BB coordinates of selected box
                 
                 if success:
                     (x,y,w,h) = [int(v) for v in box]
                     cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2) #draw green rectangle around new coordinates of tracking box
                     
                 #update fps counter
-                fps.update()
-                fps.stop()
+                self.fps.update()
+                self.fps.stop()
                 
                 #initialize info to be diplayed on frame
                 info=[
@@ -106,29 +106,20 @@ class track():
                 BBtrack=cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
                 
                 #start object tracker on the supplied bounding box, start FPS throughput estimator
-                tracker.init(frame, BBtrack)
-                fps=FPS().start()
+                self.tracker.init(frame, BBtrack)
+                self.fps=FPS().start()
             
             #if 'q' key is pressed, break
             elif key==ord("q"):
                 break
             
         if self.video == None:
-            vs.stop()#release webcam pointer
+            self.vs.stop()#release webcam pointer
         else:
-            vs.release()#release file pointer
+            self.vs.release()#release file pointer
             
         cv2.destroyAllWindows()
     
-    if __name__ == "__main__":
-        self.video = "/home/robot/abhi/Object-Tracker/cv2_test.mp4"
-        self.tracker = "csrt"
-        start()
-        
-    
-    
-    
-            
-        
-        
-    
+if __name__ == "__main__":
+	obj_tracker=track("~/Desktop/cv2_test.mp4", "csrt")
+        obj_tracker.start()
