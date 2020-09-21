921c1c183b3f563355c74ec9c547f7365d54d22a
Abhinandan Vellanki <abhi@Abhinandans-MacBook-Pro.local>
2020-09-21 14:09:24 +0800

    checking with screen

--- tracker.py
+++ tracker.py
@@ -70,19 +70,18 @@ class track():
                         cv2.putText(frame, text, (10, H - ((i * 20) + 20)),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
                 #uncomment the following block to test without screen and with input coordinates
-
+                """
                 if self.BBtrack is None:
                     (x,y,w,h) = input("Enter initial bounding box coordinates as \"[topleftX, topleftY, width, height]")
                     cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2) #draw initial ROI
 
                 #add output frame to frames list
                 self.frames.append(frame) #add frame to list of frames
-
-
+                """
                 #end of test without screen block
 
                 #uncomment the following block to test with screen
-                """
+                
                 #show output frame
                 cv2.imshow("Frame", frame)
                 key=cv2.waitKey(1) & 0xFF
@@ -99,7 +98,7 @@ class track():
                 #if 'q' key is pressed, break
                 elif key==ord("q"):
                     break
-                """
+                
                 #end of test with screen block
             else:
                 print("ERROR!! Stream could not be opened!!")
